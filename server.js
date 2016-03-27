feef3f59fafdf2cad15879c73640eb573e3931bd
Youcef Mami <youcef.mami@gmail.com>
2016-03-27 15:55:59 -0400

    Add current users command

--- server.js
+++ server.js
@@ -9,6 +9,31 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+// Sends current users to provided socket
+function sendCurrentUsers (socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	// If the room does not exist
+	if (typeof info === 'undefined') {
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current users: ' + users.join(', '),
+		timestamp: moment.valueOf()
+	});
+} 
+
 //Listen for events
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
@@ -42,14 +67,17 @@ io.on('connection', function (socket) {
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 
-		message.timestamp = moment().valueOf();
-		// send to everyone but the one who emitted the message
-		//socket.broadcast.emit('message', message);
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			// send to everyone but the one who emitted the message
+			//socket.broadcast.emit('message', message);
 
-		// io.emit sends the message to everyone
-		//io.emit('message', message);
-		//to room name
-		io.to(clientInfo[socket.id].room).emit('message', message);
+			// io.emit sends the message to everyone io.emit('message', message);
+			//to room name
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 	});
 
 	//var timestampMoment = moment.utc(timestamp).local().format('h:mm a');
